348b1522f6ce96eada0b4c0f8b61299e7bf5ee20
Gladys777 <gladysnader@hotmail.com>
2022-11-02 19:57:02 -0500

    App Música Sprint 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mi_musica/firebase_options.dart';
 import 'package:mi_musica/pages/login_page.dart';
 import 'package:mi_musica/pages/register_page.dart';
 import 'package:mi_musica/pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
